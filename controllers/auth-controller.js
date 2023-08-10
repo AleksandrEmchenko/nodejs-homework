7db27127aac0fefc1025b03bdd0d8fdaa5cea23c
Aleksandr Yemchenko <aleksandremchenko@gmail.com>
2023-08-10 12:25:24 +0300

    fix jimp

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -4,8 +4,8 @@ import jwt from "jsonwebtoken";
 import fs from "fs/promises";
 import path from "path";
 
-
 import gravatar from "gravatar";
+import Jimp from "jimp";
 
 import User from "../models/users.js";
 
@@ -13,11 +13,11 @@ import usersSchemas from "../schemas/users-schemas.js";
 ("../schemas/users-schemas.js");
 import { HttpError } from "../helpers/index.js";
 
-const {JWT_SECRET} = process.env;
+const { JWT_SECRET } = process.env;
 
 const signup = async (req, res) => {
   const { email, password } = req.body;
-  
+
   const user = await User.findOne({ email });
   if (user) {
     res.status(409).json({
@@ -25,11 +25,15 @@ const signup = async (req, res) => {
     });
     throw HttpError(409, "Email in use");
   }
-  
-  const avatarURL = gravatar.url(email, {s: '200', r: 'pg', d: '404'});
-  
+
+  const avatarURL = gravatar.url(email, { s: "200", r: "pg", d: "404" });
+
   const hashPassword = await bcrypt.hash(password, 10);
-  const newUser = await User.create({ ...req.body, avatarURL, password: hashPassword });
+  const newUser = await User.create({
+    ...req.body,
+    avatarURL,
+    password: hashPassword,
+  });
 
   res.status(201).json({
     email: newUser.email,
@@ -42,73 +46,77 @@ const signin = async (req, res) => {
   const user = await User.findOne({ email });
   if (!user) {
     res.status(401).json({
-        message: "Email or password is wrong",
-      });
+      message: "Email or password is wrong",
+    });
     throw HttpError(401, "Email or password is wrong");
   }
   const passwordCompare = await bcrypt.compare(password, user.password);
-  if(!passwordCompare) {
+  if (!passwordCompare) {
     throw HttpError(401, "Email or password is wrong");
   }
-const payload = {
+  const payload = {
     id: user._id,
-}
-  const token = jwt.sign(payload, JWT_SECRET, {expiresIn: "23h"});
-  await User.findByIdAndUpdate(user._id, {token});
+  };
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
+  await User.findByIdAndUpdate(user._id, { token });
 
   res.json({
-    "token": token,
-    "user": {
-      "email": user.email,
-      "subscription": "starter",
-  },
-});
-}
+    token: token,
+    user: {
+      email: user.email,
+      subscription: "starter",
+    },
+  });
+};
 
 const getCurrent = (req, res) => {
-  const {name, email} = req.user;
+  const { name, email } = req.user;
 
   res.json({
-    name, 
+    name,
     email,
-  })
-}
+  });
+};
 const signout = async (req, res) => {
-  const {_id} = req.user;
-  await User.findOneAndUpdate(_id, {token: ""});
+  const { _id } = req.user;
+  await User.findOneAndUpdate(_id, { token: "" });
 
   res.json({
-    message: "Singout success"
-  })
-
-}
+    message: "Singout success",
+  });
+};
 
 // change userAvatar
 const newPath = path.resolve("public", "avatars");
 
 const updateAvatar = async (req, res) => {
-  
-  const {_id} = req.user;
-  const {path: oldPath, filename} = req.file;
+  const { _id } = req.user;
+  const { path: oldPath, filename } = req.file;
   const avatarURL = path.join(newPath, filename);
-  
-  try{
+
+  try {
+    const JIMPTImg = Jimp.read(oldPath)
+      .then((avatar) => {
+        return avatar.resize(250, 250).write(newPath);
+      })
+      .catch((err) => {
+        console.error(err);
+        throw HttpError(400, "img resize error");
+      });
+    console.log("JIMPTImg", JIMPTImg);
+
     await fs.rename(oldPath, avatarURL);
-    await User.findByIdAndUpdate(_id, {avatarURL}, { new: true });
-
-
-  }
-  catch(error){
+    await User.findByIdAndUpdate(_id, { avatarURL }, { new: true });
+  } catch (error) {
     res.status(401).json({
-      "message": "Not authorized"
-    })
+      message: "Not authorized",
+    });
   }
 
   res.status(200).json({
-    "avatarURL": avatarURL,
-  })
-
-}
+    avatarURL: avatarURL,
+  });
+};
 
 export default {
   signup,
